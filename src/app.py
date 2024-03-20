3bd6abd0f5705e0f0a2579537687e1aa5fab447a
NajlaF <134140635+NajlaF@users.noreply.github.com>
2024-03-20 08:43:03 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,118 +1,38 @@
-import dash
-import dash_bootstrap_components as dbc
+'''
+ # @ Create Time: 2024-03-13 12:12:32.711699
+'''
+
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
-import plotly.graph_objs as go
-from dash import Input, Output, dcc, html
-from sklearn import datasets
-from sklearn.cluster import KMeans
 
-iris_raw = datasets.load_iris()
-iris = pd.DataFrame(iris_raw["data"], columns=iris_raw["feature_names"])
-
-app = dash.Dash(
-    title="Dpage",
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-)
+app = Dash(__name__, title="retest")
 
 # Declare server for Heroku deployment. Needed for Procfile.
 server = app.server
 
-controls = dbc.Card(
-    [
-        html.Div(
-            [
-                dbc.Label("تاريخ"),
-                dcc.Dropdown(
-                    id="x-variable",
-                    options=[
-                        {"label": col, "value": col} for col in iris.columns
-                    ],
-                    value="sepal length (cm)",
-                ),
-            ]
-        ),
-        html.Div(
-            [
-                dbc.Label("عدد البروتينات"),
-                dbc.Input(id="cluster-count", type="number", value=3),
-            ]
-        ),
-    ],
-    body=True,
-)
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
-app.layout = dbc.Container(
-    [
-        html.H1("تجميع البروتينات في زهرة الآيرس"),
-        html.Hr(),
-        dbc.Row(
-            [
-                dbc.Col(controls, md=4),
-                dbc.Col(dcc.Graph(id="cluster-graph"), md=8),
-            ],
-            align="center",
-        ),
-    ],
-    fluid=True,
-)
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
+app.layout = html.Div(children=[
+    html.H1(children='hi Najla'),
 
-@app.callback(
-    Output("cluster-graph", "figure"),
-    [
-        Input("x-variable", "value"),
-        Input("cluster-count", "value"),
-    ],
-)
-def make_graph(x, n_clusters):
-    # التحقق الأدنى من المدخلات، تأكد من وجود عدد البروتينات المطلوب على الأقل
-    km = KMeans(n_clusters=max(n_clusters, 1))
-    df = iris.loc[:, [x]]
-    km.fit(df.values)
-    df["cluster"] = km.labels_
+    html.Div(children='''
+        i hupe you will change your beh.
+    '''),
 
-    centers = km.cluster_centers_
-
-    data = [
-        go.Scatter(
-            x=df.loc[df.cluster == c, x],
-            y=df.loc[df.cluster == c, x],
-            mode="markers",
-            marker={"size": 8},
-            name="Cluster {}".format(c),
-        )
-        for c in range(n_clusters)
-    ]
-
-    data.append(
-        go.Scatter(
-            x=centers[:, 0],
-            y=centers[:, 0],
-            mode="markers",
-            marker={"color": "#000", "size": 12, "symbol": "diamond"},
-            name="مراكز الأcluster",
-        )
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
     )
+])
 
-    layout = {"xaxis": {"title": x}, "yaxis": {"title": "عدد البروتينات"}}
-
-    return go.Figure(data=data, layout=layout)
-
-
-# التأكد من أن قيم x و y لا يمكن أن تكونا نفس المتغير
-def filter_options(v):
-    """تعطيل الخيار v"""
-    return [
-        {"label": col, "value": col, "disabled": col == v}
-        for col in iris.columns
-    ]
-
-
-# نفس الوظيفة لكل من القائمتين المنسدلتين، لذا نعيد استخدام filter_options
-app.callback(Output("x-variable", "options"), [Input("cluster-count", "value")])(
-    filter_options
-)
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True, port=8050)
+if __name__ == '__main__':
+    app.run_server(debug=True)
